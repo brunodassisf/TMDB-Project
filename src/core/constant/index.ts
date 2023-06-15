355ef834c51ef5655090e43879e3447d1d9816db
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-06-15 01:59:22 -0300

    refactor constant and interfaces

--- src/core/constant/index.ts
+++ src/core/constant/index.ts
@@ -1,4 +1,9 @@
 export const FETCH_API = {
+  POPULAR: `${process.env.NEXT_PUBLIC_API_URL}/movie/popular?language=pt-BR`,
   POPULAR_MOVIE: `${process.env.NEXT_PUBLIC_API_URL}/movie/popular?language=pt-BR`,
   POPULAR_TV: `${process.env.NEXT_PUBLIC_API_URL}/tv/popular?language=pt-BR`,
+};
+
+export const FECTH_POPULAR = (type: string) => {
+  return `${process.env.NEXT_PUBLIC_API_URL}/${type}/popular?language=pt-BR`;
 };
--- src/core/helper/interface/index.ts
+++ src/core/helper/interface/index.ts
@@ -1,6 +1,8 @@
 export interface IFetchData<T> {
   page: number;
   results: T[];
+  total_pages: number;
+  total_results: number;
 }
 
 export interface IMovie {
